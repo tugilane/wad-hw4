aa211642cd69dc341177673c1a69c045d8d766ce
tugilane <marten.peljo@gmail.com>
2025-12-13 23:38:20 +0200

    signup/login page look okay now

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="form">
-    <h3>LogIn</h3>
-    <label for="email">Email</label>
-    <input type="email" name="email"  required v-model="email">
-    <label for="password">Password</label>
-    <input type="password" name="password" required v-model="password">
-    <div class="container">
-      <button @click="LogIn"  class="center">Login</button>
-      <button @click='this.$router.push("/signup")' class="center">Signup</button>
-    </div>
+  <div>
+    <h2>Log in</h2>
+        <form class="signup-form">
+      <input type="email" placeholder="Email" required v-model="email" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <div>
+      <button id="loginBtn" @click="LogIn" class="center">Login</button>
+      <button id="signUPfLoginBtn" @click='this.$router.push("/signup")' class="center">Signup</button>
+      </div>
+    </form>
+    
   </div>
 </template>
 
@@ -55,56 +56,17 @@ LogIn() {
 
 </script>
 
-<style scoped>
-.form {
-  max-width: 420px;
-  margin: 30px auto;
-  background: rgb(167, 154, 154);
-  text-align: left;
-  padding: 40px;
-  border-radius: 10px;
+<style>
+
+#loginBtn {
+    width: 40%;
+
 }
-h3 {
-  text-align: center;
-  color: rgb(8, 110, 110);
+
+#signUPfLoginBtn {
+  margin-left: 20%;
+  width: 40%;
 }
-label {
-  color: rgb(8, 110, 110);
-  display: inline-block;
-  margin: 25px 0 15px;
-  font-size: 0.8em;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  font-weight: bold;
-}
-input {
-  display: block;
-  padding: 10px 6px;
-  width: 100%;
-  box-sizing: border-box;
-  border: none;
-  border-bottom: 1px solid white;
-  color: blue;
-}
-button {
-  background: rgb(8, 110, 110);
-  border: 0;
-  padding: 10px 20px;
-  margin: 20px 20px 20px 20px;
-  color: white;
-  border-radius: 20px;
-  align-items: center;
-  text-align: center;
-}
-.center {
-  margin: auto;
-  border: 0;
-  padding: 10px 20px;
-  margin-top: 20px;
-  width: 30%; 
-}
-.container {
-  display: flex;
-  justify-content: center;
-}
+
+
 </style>
